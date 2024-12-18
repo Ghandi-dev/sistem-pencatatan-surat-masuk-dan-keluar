f2a1db2a9f31b0bd60649b0d3e4b023c2ee58b5b
Ghandi-dev <scoutghandi31@gmail.com>
2024-12-18 19:26:53 +0700

    footer

--- application/views/component/footer.php
+++ application/views/component/footer.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
     <div class="footer-left">
-        Copyright &copy; 2024. <div class="bullet"></div> by <a href="#"></a>
+        Copyright &copy; 2024. <div class="bullet"></div> by Salsabila <a href="#"></a>
     </div>
     <div class="footer-right">
 
